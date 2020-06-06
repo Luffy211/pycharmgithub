4f8b9ef01fc56c83c27e5dc214255c226c1b90ad
Luffy211 <892442464@qq.com>
2020-06-06 16:59:12 +0800

    22

--- src/TT.java
+++ src/TT.java
@@ -1,2 +1,7 @@
 public class TT {
+    public static void main(String[] args) {
+        String str1 = "the light";
+        String str3 = "the light";
+        System.out.println( str1  ==  str3);
+    }
 }
